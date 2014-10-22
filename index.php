a4524fe39b2b8f6102f0ff964ae8f255dfbeb631
renfeisong <renfei.song@gmail.com>
2014-10-22 13:01:21 +0800

    added  and  event

--- index.php
+++ index.php
@@ -12,10 +12,18 @@ $receiver = new MessageReceiver();
 
 $receiver->receive();
 
+$catched = false;
+
 foreach ($modules as $module) {
     /* @var $module BaseModule */
     if ($module->can_handle_input($receiver->input)) {
+        $catched = true;
+        do_actions('module_hit', array($receiver->input, get_class($module)));
         echo $module->raw_output($receiver->input);
         break;
     }
+}
+
+if ($catched == false) {
+    do_actions('modules_missed', array($receiver->input));
 }
