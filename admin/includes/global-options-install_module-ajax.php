d0e9abb0e8eab74b320addd81a6aa76ae0481f1d
qinbingchen <qinbingchen@me.com>
2014-11-05 04:27:06 +0800

    0.7 implementation of upload module.

---
+++ admin/includes/global-options-install_module-ajax.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * This file provides ajax services for the user management page.
+ *
+ * @author Bingchen Qin
+ * @since 2.0.0
+ *
+ * Error list:
+ *
+ * 0 success
+ * 1 upload error
+ * 2 size limit exceeded
+ * 3 type not zip
+ * 4 already exists
+ * 5 extract error
+ * 6 file names not identical
+ * 7 index.php not found
+ * 8 class name and file name not identical
+ * 9 not subclass of BaseClass
+ *
+ * 100 unknown error
+ */
+
+require_once dirname(__FILE__) . "/admin.php";
+global $wxdb;
+
+$module_dir = dirname(dirname(dirname(__FILE__))) . "/modules/";
+
+$return_dict = array();
+$error_message = array(0 => "success",
+                       1 => "upload error",
+                       2 => "size limit exceeded",
+                       3 => "type not zip",
+                       4 => "already exists",
+                       5 => "extract error",
+                       6 => "file names not identical",
+                       7 => "index.php not found",
+                       8 => "class name and file name not identical",
+                       9 => "not subclass of BaseClass",
+                     100 => "unknown error");
+
+if (!isset($_FILES["file"]["error"]) || is_array($_FILES["file"]["error"])) {
+    $return_dict["code"] = 1;
+    goto complete;
+}
+
+if ($_FILES["file"]["error"] == UPLOAD_ERR_INI_SIZE || $_FILES["file"]["error"] == UPLOAD_ERR_FORM_SIZE) {
+    $return_dict["code"] = 2;
+    goto complete;
+}
+
+if ($_FILES["file"]["error"] == UPLOAD_ERR_OK) {
+    $module_name = basename($_FILES["file"]["name"], ".zip");
+    $temp_dir = dirname(dirname(dirname(__FILE__))) . "/temp/" . $module_name . "_" . md5(rand(1000, 1000000)) . "_" . date("YmdHis", time()) . "/";
+    $temp_file = $temp_dir . $_FILES["file"]["name"];
+
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $file_type = finfo_file($finfo, $_FILES["file"]['tmp_name']);
+    finfo_close($finfo);
+    if ($file_type != "application/zip") {
+        $return_dict["code"] = 3;
+        goto complete;
+    }
+
+    if (file_exists($module_dir . $module_name)) {
+        $return_dict["code"] = 4;
+        goto complete;
+    }
+
+    mkdir($temp_dir);
+    move_uploaded_file($_FILES["file"]["tmp_name"], $temp_file);
+    $zip = new ZipArchive();
+    if ($zip->open($temp_file) !== true) {
+        $return_dict["code"] = 5;
+        goto complete;
+    }
+
+    if (!$zip->extractTo($temp_dir)) {
+        $return_dict["code"] = 5;
+        goto complete;
+    }
+
+    $zip->close();
+    //unlink($temp_file);
+    if (!file_exists($temp_dir . $module_name)) {
+        $return_dict["code"] = 6;
+        goto complete;
+    }
+
+    if (!file_exists($temp_dir . $module_name . "/index.php")) {
+        $return_dict["code"] = 7;
+        goto complete;
+    }
+
+    ob_start();
+    require_once($temp_dir . $module_name . "/index.php");
+    ob_end_clean();
+    if (!class_exists($module_name)) {
+        $return_dict["code"] = 8;
+        goto complete;
+    }
+
+    $module = new $module_name;
+    if (!is_subclass_of($module, "BaseModule")) {
+        $return_dict["code"] = 9;
+        goto complete;
+    }
+
+    rename($temp_dir . $module_name, $module_dir . $module_name);
+    $return_dict["code"] = 0;
+    goto complete;
+}
+
+$return_dict["code"] = 1;
+
+complete:
+$return_dict["message"] = $error_message[$return_dict["code"]];
+echo json_encode($return_dict);
+
+?>
--- admin/includes/global-options-install_module.php
+++ admin/includes/global-options-install_module.php
@@ -1,2 +1,24 @@
 <?php
-echo 'install';
+/**
+ * This file is the install module page for the admin center.
+ *
+ * @author Bingchen Qin
+ * @since 2.0.0
+ */
+
+global $wxdb; /* @var $wxdb wxdb */
+
+$result = $wxdb->get_results('SELECT * FROM admin_user', ARRAY_A);
+
+?>
+
+<h2>安装模块</h2>
+
+<form action="./includes/global-options-install_module-ajax.php" method="post" enctype="multipart/form-data">
+<!--    <input type="file" name="file">-->
+<!--    <button class="button red-button" type="submit">选择文件...</button>-->
+
+    <label for="file">Filename:</label>
+    <input type="file" name="file" id="file" />
+    <input type="submit" name="submit" value="Submit" />
+</form>
