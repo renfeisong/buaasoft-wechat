e68b383be05e97d8641c3425580c8453ba81c741
renfeisong <renfei.song@gmail.com>
2014-12-02 02:13:25 +0800

    Add security log for module priority change

--- admin/includes/global-options-modules-ajax.php
+++ admin/includes/global-options-modules-ajax.php
@@ -1,12 +1,13 @@
 <?php
-
 /**
  * Handling AJAX request from Module Settings Panel (global-options-modules.php)
  *
  * @author Renfei Song
  */
 
-require_once dirname(dirname(dirname(__FILE__))) . '/config.php';
+require_once dirname(__FILE__) . "/admin.php";
+
+global $wxdb; /* @var $wxdb wxdb */
 
 // Security check
 $ajax_key = get_global_value('options_module_ajax');
@@ -17,9 +18,17 @@ if (sha1(AJAX_SALT . $ajax_key) != @$_GET['auth']) {
 }
 
 if (strval(intval($_POST['value'])) === $_POST['value']) {
+    $old_value = get_global_value('priority_' . $_POST['pk']);
     $value = intval($_POST['value']);
     if ($value >= 0 && $value <= 65535) {
         set_global_value('priority_' . $_POST['pk'], $value);
+        $wxdb->insert('security_log', array(
+            'userName' => current_user_name(),
+            'opName' => 'Module.setPriority',
+            'opDetail' => 'Success: Priority for module [' . $_POST["pk"] . '] set from [' . $old_value . '] to [' . $value . ']',
+            'ip' => $_SERVER['REMOTE_ADDR'],
+            'agent' => $_SERVER['HTTP_USER_AGENT']
+        ));
         exit;
     }
 }
