d87daef5e47438a56756673a010b9405ec0a5190
qinbingchen <qinbingchen@me.com>
2014-12-05 18:47:21 +0800

    Contact table now can be edited.

--- modules/Contact/ajax.php
+++ modules/Contact/ajax.php
@@ -10,6 +10,7 @@ require_once dirname(dirname(dirname(__FILE__))) . '/config.php';
 
 if (isset($_POST["action"])) {
     $return_dict = array();
+    global $wxdb; /* @var $wxdb wxdb */
 
     switch ($_POST["action"]) {
         case "edit": {
@@ -18,11 +19,51 @@ if (isset($_POST["action"])) {
             $return_dict["message"] = "success";
             break;
         }
+        case "edit-user-name": {
+            if ($wxdb->update("contact", array("userName"=>$_POST["user_name"]), array("id"=>$_POST["id"])) != false) {
+                $return_dict["code"] = 0;
+                $return_dict["message"] = "success";
+            } else {
+                $return_dict["code"] = 1;
+                $return_dict["message"] = "error";
+            }
+            break;
+        }
+        case "edit-identity": {
+            if ($wxdb->update("contact", array("identity"=>$_POST["identity"]), array("id"=>$_POST["id"])) != false) {
+                $return_dict["code"] = 0;
+                $return_dict["message"] = "success";
+            } else {
+                $return_dict["code"] = 1;
+                $return_dict["message"] = "error";
+            }
+            break;
+        }
+        case "edit-phone-number": {
+            if ($wxdb->update("contact", array("phoneNumber"=>$_POST["phone_number"]), array("id"=>$_POST["id"])) != false) {
+                $return_dict["code"] = 0;
+                $return_dict["message"] = "success";
+            } else {
+                $return_dict["code"] = 1;
+                $return_dict["message"] = "error";
+            }
+            break;
+        }
+        case "edit-email": {
+            if ($wxdb->update("contact", array("email"=>$_POST["email"]), array("id"=>$_POST["id"])) != false) {
+                $return_dict["code"] = 0;
+                $return_dict["message"] = "success";
+            } else {
+                $return_dict["code"] = 1;
+                $return_dict["message"] = "error";
+            }
+            break;
+        }
         default: {
             $return_dict["code"] = 1;
             $return_dict["message"] = "error";
             break;
         }
-        echo json_encode($return_dict);
     }
+    echo json_encode($return_dict);
 }
--- modules/Contact/settings.php
+++ modules/Contact/settings.php
@@ -9,6 +9,9 @@
 require_once dirname(dirname(dirname(__FILE__))) . '/config.php';
 $format = _get_value("Contact", "output_format");
 
+global $wxdb; /* @var $wxdb wxdb */
+$results = $wxdb->get_results("SELECT * FROM contact", ARRAY_A);
+
 ?>
 
 <style>
@@ -44,6 +47,46 @@ $format = _get_value("Contact", "output_format");
 
 
 <h2>通讯信息查询管理</h2>
+
+<h3>Contact表管理</h3>
+<table id="user-table" class="table table-striped table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>姓名</th>
+        <th>身份</th>
+        <th>电话号码</th>
+        <th>邮箱</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($results as $row):?>
+        <tr data-id="<?php echo $row["id"] ?>">
+            <td>
+                <a href="#" class="x-editable x-editable-user-name">
+                    <?php echo $row["userName"] ?>
+                </a>
+            </td>
+            <td>
+                <a href="#" class="x-editable x-editable-identity">
+                    <?php echo $row['identity'] ?>
+                </a>
+            </td>
+            <td>
+                <a href="#" class="x-editable x-editable-phone-number">
+                    <?php echo $row['phoneNumber'] ?>
+                </a>
+            </td>
+            <td>
+                <a href="#" class="x-editable x-editable-email">
+                    <?php echo $row['email'] ?>
+                </a>
+            </td>
+        </tr>
+    <?php endforeach;?>
+    </tbody>
+</table>
+
+
 <h3>展示信息管理</h3>
 
 <textarea id="format" class="form-control" rows="5"><?php echo $format ?></textarea>
@@ -67,9 +110,123 @@ $format = _get_value("Contact", "output_format");
 <button id="submitting" class="button blue-button button-with-icon hidden"><i class="fa fa-spinner fa-spin fa-fw"></i> 正在提交...</button>
 <button id="success" class="button green-button button-with-icon hidden"><i class="fa fa-check fa-fw"></i> 修改成功</button>
 
+
 <script>
 
     $(document).ready(function() {
+
+        $(".x-editable").editable({
+            type: "text",
+            emptytext: "点击添加..."
+        });
+
+        $(".x-editable-user-name").on("save", function(e, params) {
+            $.ajax({
+                url: "../modules/Contact/ajax.php",
+                type: "POST",
+                dataType: "json",
+                data: {
+                    "action": "edit-user-name",
+                    "id": $(this).parents("tr").data("id"),
+                    "user_name": params.newValue
+                }
+            }).done(function(data){
+                switch (data["code"]) {
+                    case 0: {
+                        toastr.success("修改成功", "Success");
+                        break;
+                    }
+                    case 1: {
+                        toastr.error("系统错误", "Error");
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            });
+        });
+
+        $(".x-editable-identity").on("save", function(e, params) {
+            $.ajax({
+                url: "../modules/Contact/ajax.php",
+                type: "POST",
+                dataType: "json",
+                data: {
+                    "action": "edit-identity",
+                    "id": $(this).parents("tr").data("id"),
+                    "identity": params.newValue
+                }
+            }).done(function(data){
+                switch (data["code"]) {
+                    case 0: {
+                        toastr.success("修改成功", "Success");
+                        break;
+                    }
+                    case 1: {
+                        toastr.error("系统错误", "Error");
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            });
+        });
+
+        $(".x-editable-phone-number").on("save", function(e, params) {
+            $.ajax({
+                url: "../modules/Contact/ajax.php",
+                type: "POST",
+                dataType: "json",
+                data: {
+                    "action": "edit-phone-number",
+                    "id": $(this).parents("tr").data("id"),
+                    "phone_number": params.newValue
+                }
+            }).done(function(data){
+                switch (data["code"]) {
+                    case 0: {
+                        toastr.success("修改成功", "Success");
+                        break;
+                    }
+                    case 1: {
+                        toastr.error("系统错误", "Error");
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            });
+        });
+
+        $(".x-editable-email").on("save", function(e, params) {
+            $.ajax({
+                url: "../modules/Contact/ajax.php",
+                type: "POST",
+                dataType: "json",
+                data: {
+                    "action": "edit-email",
+                    "id": $(this).parents("tr").data("id"),
+                    "email": params.newValue
+                }
+            }).done(function(data){
+                switch (data["code"]) {
+                    case 0: {
+                        toastr.success("修改成功", "Success");
+                        break;
+                    }
+                    case 1: {
+                        toastr.error("系统错误", "Error");
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            });
+        });
 
         $("#submit").click(function() {
             var format = $("#format").val();
